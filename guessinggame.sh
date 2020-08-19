1d03af6119c406933eed435aa2e2be71003f195b
keerthana-kv123 <68042107+keerthana-kv123@users.noreply.github.com>
2020-08-19 13:57:18 +0530

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,27 @@
+## Guessing Game##
+#User will try to guess the number of files in the directory#
+
+
+choice=-1
+correct=7
+typeset -i num=0
+
+#Game Begins
+echo "How many files are in the working directory?"
+
+###Answer
+(( answer = 7))
+
+while (( choice !=answer )); do
+        num=num+1
+        read -p "Take a guess $num: " choice
+        if (( choice < correct )); then
+                echo "Too Low"
+                echo "Try again"
+        elif (( choice > correct )); then
+                echo "Too High"
+                echo "Try again"
+        fi
+done
+#Finish
+echo "CONGRATULATIONS!!You guessed it right!!"
